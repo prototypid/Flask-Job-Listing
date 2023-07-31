6636a571f46f8f5764521dba148b9d1143236223
darq <prototypid@gmail.com>
2023-07-31 16:46:24 +0530

    Render template & return json data

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Structure',
+        'location': 'Delhi, India',
+        'salary': 'Rs. 15.00.00',
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': 'Rs 12,00,000',
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$120,000'
+    }
+]
+
 
 @app.route("/")
-def hello_world():
-    return render_template('page.html')
+def render_page():
+    return render_template('page.html', jobs=JOBS)
+
+
+@app.route("/api/v1/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
